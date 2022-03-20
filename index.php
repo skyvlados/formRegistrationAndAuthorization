006dbb49b0990c88ccf7a181a9fda9abff81b9c9
vladislav.rep <vladislav.rep@umbrellio.biz>
2022-03-20 12:39:22 +0300

    second step

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-
 require __DIR__ . '/auth.php';
 $login = getUserLogin();
+if ($login === null) {
+    header("Location: /login.php");
+    exit;
+}
 ?>
 <html>
 <head>
     <title>Главная страница</title>
 </head>
 <body>
-<?php if ($login === null): ?>
-    <a href="/login.php">Авторизуйтесь</a>
-<?php else: ?>
-    Добро пожаловать, <?= $login ?>
+    Добро пожаловать, <?php echo $login ?>
     <br>
     <a href="/logout.php">Выйти</a>
-<?php endif; ?>
 </body>
 </html>
